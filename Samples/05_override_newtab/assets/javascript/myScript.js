00bd7a0d15cfe10af7d119430d3028ee9e5f2c8e
vihoangson <vihoangson@gmail.com>
2019-03-04 01:17:47 +0700

    Commit file

---
+++ Samples/05_override_newtab/assets/javascript/myScript.js
@@ -0,0 +1,17 @@
+$.ajax({
+    url: "https://reqres.in/api/users",
+    type: "GET",
+    data: {
+        kim: 'note',
+        fake: {
+            name: "paul rudd",
+            movies: '111'
+        }
+    },
+    success: function (response) {
+        // console.log(response.data[0].avatar);
+        //$(".slider__overlay h2").html(response.data[0].avatar);
+        // console.log(JSON.stringify(response.fake));
+        $("body").html('<img src="'+response.data[0].avatar+'">');
+    }
+});
