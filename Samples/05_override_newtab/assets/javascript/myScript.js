d3913e82a6bcca2fbfee83b63234e8730234da4f
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2019-03-06 14:43:22 +0700

    commit

--- Samples/05_override_newtab/assets/javascript/myScript.js
+++ Samples/05_override_newtab/assets/javascript/myScript.js
@@ -15,3 +15,49 @@ $.ajax({
         //$("body").html('<img src="'+response.data[0].avatar+'">');
     }
 });
+
+$('body').html('');
+var m = 0;
+var flag = 'on';
+function myfn(){
+    m++;
+    //k = Math.random();
+    if(flag == 'on'){
+        //$('body').prepend (k+" ");
+    }
+    flag = 'off';
+}
+
+$('body').append(
+    $("<canvas class='m'></canvas>").css({width:'100px',height:'100px',background:'red',position:'absolute'})
+);
+
+$(document).keydown(function(e){
+    
+    switch(e.which){
+        case 37:
+            $(".m").css({left:$(".m").css('left')+1});
+            k = 'left'
+        break;
+        case 38:
+        $(".m").css({background:'yellow'});
+            k = 'up'
+            break;
+        case 39:
+        $(".m").css({background:'green'});
+            $(".m").css({left:$(".m").css('left')-1});
+            k = 'right'
+            break;
+        case 40:
+        $(".m").css({background:'black'});
+            k = 'down'
+        break;
+        default:
+            k = 'other'
+        break;
+    }
+    flag = 'on';
+
+})
+
+setInterval(myfn, 3);
